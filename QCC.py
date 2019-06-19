89472d5995ef3826f69b3504e1e8eea1984c24f7
sdtcsyl <sdtcsyl@163.com>
2019-06-19 08:56:15 +0800

    Update QCC.py

--- QCC.py
+++ QCC.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun May  5 17:23:13 2019
-
-@author: e0306210
 """
 class QCC:
     def __init__(self):
@@ -19,6 +17,7 @@ class QCC:
                 'Hm_lpvt_3456bee468c83cc63fb5147f119f1075':'1560776190',  
                 'acw_sc__v3':'5d0792faed04b77595fed427c078638496952010',
                 'acw_sc__v2':'5d0792f7410b8da59a86353fe1f0a235d4666f70',
+                # the below item will change every time. the above items still stay the same when they are set at the first time.
                 'zg_de1d1a35bfa24ce29bbf2c7eb17e6c4f':'%7B%22sid%22%3A%201560867253543%2C%22updated%22%3A%201560867265043%2C%22info%22%3A%201560776045549%2C%22superProperty%22%3A%20%22%7B%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22%22%2C%22cuid%22%3A%20%224c2c67fe8492e168ff683117aedcc767%22%7D'
                 }
         
